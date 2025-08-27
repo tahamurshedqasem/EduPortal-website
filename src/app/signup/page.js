d053ea5ef5a9acb07cceb63ae31b22a231966a7e
Taha Murshed <taha.m.qasem@gmail.com>
2025-08-28 02:14:34 +0300

    Next.js frontend

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -57,7 +57,7 @@ export default function SignupPage() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/register", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
